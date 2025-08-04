449b9fda5d0075648fa945a09fb96ce4d4bce1cb
ryzenop07 <ajay211st@gmail.com>
2025-08-04 13:36:24 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+from telegram.ext import Updater
+from handlers import *
+
+from telegram.ext import CommandHandler
+from config import BOT_TOKEN
+
+def main():
+    updater = Updater(BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("newgame", new_game))
+    dp.add_handler(CommandHandler("join", join))
+    dp.add_handler(CommandHandler("players", show_players))
+    dp.add_handler(CommandHandler("begin", begin))
+    dp.add_handler(CommandHandler("truth", truth))
+    dp.add_handler(CommandHandler("dare", dare))
+    dp.add_handler(CommandHandler("next", next_turn))
+    dp.add_handler(CommandHandler("end", end_game))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
